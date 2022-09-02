2a67a7d6efc4d19c89218af132752de4808fa11b
khaledOPC <59145249+khaledOPC@users.noreply.github.com>
2022-09-02 15:53:09 +0200

    dépôt du fichier html
    Ici se trouve le code html avec le code js

---
+++ templates/code op html.js
@@ -0,0 +1,55 @@
+
+
+    <figure>
+        <img src="photo/aigle.jpeg" class="imageflottante" alt="Voici ma photo">
+        <p>Ce test est à coté de l'image floante</p>
+        <p class="dessous"> Photo d'un aigle</p>
+    </figure>
+
+    <p>
+        <ul>
+            <h2><li>Mon expérience</li></h2>
+            <p class="Para1">
+                J'ai 20ans d'experience  
+            </p>
+
+            <h2><li>Mes compétences</li> </h2>
+            <p class="Para2">
+                Je fais des programme 
+            </p>
+            <h2><li>Ma formation</li></h2>
+            <p class="Para3">
+                Formation python
+            </p>
+        </ul>
+    </p>
+
+    <p>
+    <form method="post" action="traitement.php">
+        <p>
+            <label for="pseudo">Votre pseudo :</label>
+            <input type="text" name="pseudo" id="pseudo" />
+       
+            <br/>
+            <label for="pass">Votre mot de passe :</label>
+            <input type="password" name="pass" id="pass" />
+       
+        </p>
+        </form>
+
+
+
+        <label>Entrez un mail : <input type="texte" name="pseudo" id="pseudo" placeholder="Ex : monmail@gmail.com" size="33"></label>
+    <form method="post" action="traitement.php">
+
+        <label for="pass">Votre mot de passe :</label>
+        <input type="password" name="pass" id="pass" />
+    
+    </form>
+    </p>
+
+
+
+
+
+
