6781e3c6d7b6375642f166fabcc2ed06a8f9423c
khaledOPC <59145249+khaledOPC@users.noreply.github.com>
2022-09-02 15:35:38 +0200

    Dépôt du fichier main.py
    Code flask / python

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from flask import Flask, jsonify, make_response, render_template, request
+import wikipedia
+import os
+from function import clean_search
+from function import search_google
+from function import search_wikipedia
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+
+@app.route("/message", methods=["POST"])
+def message():
+    search = clean_search(request.form["search"])
+    response = search_google(search)
+    if not response:
+        return make_response("aucune information trouvée")
+    lat, lng, adresse = response
+    wiki_message = search_wikipedia(lat, lng)
+    return make_response(
+        jsonify({"lat": lat, "lng": lng, "adresse": adresse, "message": wiki_message})
+    )
+
+
+if __name__ == "__main__":
+    app.run()
